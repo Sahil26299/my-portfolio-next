dcbba62251d53f73c895251af937a70ee42e0890
Sahil Dattatray Lokhande <104907525+Sahil26299@users.noreply.github.com>
2025-12-01 13:47:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,32 @@ import { poppins } from "@/src/utilities";
 
 export const metadata: Metadata = {
   title: "Portfolio | Sahil Lokhande",
-  description: "Software developer based in Pune, india",
+  description: "Software developer based in Pune, india.",
+  openGraph: {
+    title: "Portfolio | Sahil Lokhande",
+    description:
+      "Software developer based in Pune, india.",
+    type: "website",
+    locale: "en_US",
+    siteName: "Portfolio | Sahil Lokhande",
+
+    // Ideal OG image size: 1200×630
+    images: [
+      {
+        url: "https://my-portfolio-next-mauve.vercel.app/images/appImageBg.png",
+        width: 1200,
+        height: 630,
+        alt: "Portfolio | Sahil Lokhande Background Image",
+      }
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Portfolio | Sahil Lokhande",
+    description:
+      "Software developer based in Pune, india.",
+    images: ["https://my-portfolio-next-mauve.vercel.app/images/appImageBg.png"],
+  },
 };
 
 export default function RootLayout({
